5a2af5732596d6199bd22ff4de4114ad5630277e
The Doom Lab <buchananlab@gmail.com>
2022-01-18 21:09:08 -0600

    that's right a shiny app

---
+++ 04_Procedure/shiny/app.R
@@ -0,0 +1,79 @@
+# Load Libraries ----------------------------------------------------------
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(rio)
+
+# Load Stuff --------------------------------------------------------------
+
+source("tabs.R")
+source("load_summary.R")
+
+# Define the UI -----------------------------------------------------------
+
+ui <- dashboardPage(skin = 'purple',
+                    dashboardHeader(title = "PSA 007 Tracker"),
+                    dashboardSidebar(
+                        sidebarMenu(
+                            menuItem(tags$b("English"), tabName = "english_tab")
+                            )
+                        ),
+                    dashboardBody(
+                        
+                        ## add a custom css file
+                        tags$head(tags$style(HTML('
+      .main-header .logo {
+        font-weight: bold;
+        font-size: 16px;
+      }
+      .box.box-solid.box-primary>.box-header {
+        color:#fff;
+        background:#666666
+      }
+      .box.box-solid.box-primary {
+        border-bottom-color:#666666;
+        border-left-color:#666666;
+        border-right-color:#666666;
+        border-top-color:#666666;
+      }'))),
+                        
+                        ## show the tab items
+                        tabItems(
+                          english_tab
+                        ) # end tabItems
+                    ) # end dashboardBody
+                ) # end dashboardPage
+
+# Define the Server Logic -------------------------------------------------
+server <- function(input, output) {
+    
+    output$en_participant_data <- renderDT({
+
+        colnames(en_participants) <- c("PSA_ID", "Time")
+        datatable(en_participants, rownames = F,
+                  filter = "top",
+                  options = list(dom = 'tp'))
+    })
+    
+    output$en_participant_table <- renderDT({
+      
+      temp <- as.data.frame(table(en_participants$url_lab))
+      
+      colnames(temp) <- c("PSA_ID", "Frequency")
+      datatable(temp, rownames = F,
+                filter = "top",
+                options = list(dom = 'tp'))
+    })
+    
+    output$en_summary_table <- renderDT({
+      
+      datatable(en_summary, rownames = F,
+                filter = "top",
+                options = list(dom = 'tp'))
+    })
+    
+}
+
+
+# Run the App -------------------------------------------------------------
+shinyApp(ui = ui, server = server)
---
+++ 04_Procedure/shiny/load_summary.R
@@ -0,0 +1,7 @@
+# English -----------------------------------------------------------------
+
+#en_participants <- import("/Users/erinbuchanan/GitHub/Research/2 projects/SPAML/SPAML-PSA/04_Procedure/en/data/en_participants.csv")
+#en_summary <- import("/Users/erinbuchanan/GitHub/Research/2 projects/SPAML/SPAML-PSA/04_Procedure/en/data/en_summary.csv")
+
+en_participants <- import("/var/www/html/en/data/en_participants.csv")
+en_summary <- import("/var/www/html/en/data/en_summary.csv")
