4ef22c868a6e8f7e75e0f6583ecaa0a2a35c3e2f
The Doom Lab <buchananlab@gmail.com>
2022-12-07 11:22:04 -0600

    updating danish

--- 04_Procedure/summary_data/da_summarize_stim_backup.R
+++ 04_Procedure/summary_data/da_summarize_stim_backup.R
@@ -390,8 +390,8 @@ da_data_all <- da_data_all %>%
   da_merged$done <- da_merged$sampleN >= 50
 
 # use data ----
-  da_use <- subset(da_merged, is.na(done) | done == FALSE)
-  da_sample <- subset(da_merged, done == TRUE)
+  da_use <- subset(da_merged, is.na(done_totalN) | done_totalN == FALSE)
+  da_sample <- subset(da_merged, done_totalN == TRUE)
 
 # Generate ----------------------------------------------------------------
 
