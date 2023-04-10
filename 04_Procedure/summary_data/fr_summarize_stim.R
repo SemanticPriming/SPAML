726710887e319a63a7d43513a76b485336573a70
The Doom Lab <buchananlab@gmail.com>
2023-04-10 16:48:55 -0500

    adding french updates

--- 04_Procedure/summary_data/fr_summarize_stim.R
+++ 04_Procedure/summary_data/fr_summarize_stim.R
@@ -169,6 +169,8 @@ fr_data_all <-
   bind_rows(processData("/var/www/html/fr/data/data.sqlite") %>%
               mutate(url_lab = as.character(url_lab)),
             processData("/var/www/html/fr1/data/data.sqlite") %>%
+              mutate(url_lab = as.character(url_lab)),
+            processData("/var/www/html/fr2/data/data.sqlite") %>%
               mutate(url_lab = as.character(url_lab))) %>% unique()
 
 # # delete stuff before we started
@@ -210,7 +212,7 @@ fr_data_all <- fr_data_all %>%
 # Participant did not complete at least 100 trials.
 # Participant did not achieve 80% correct.
 current_year <- 2023
-number_folders <- 2
+number_folders <- 3
 
 ##create demographics only data
 demos <- fr_data_all %>% #data frame
