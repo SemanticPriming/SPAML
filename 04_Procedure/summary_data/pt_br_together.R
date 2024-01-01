1b9d2674c5a6fefc17d5149e124478c650f855b8
The Doom Lab <buchananlab@gmail.com>
2023-12-31 20:56:28 -0500

    upload for prolific

--- 04_Procedure/summary_data/pt_br_together.R
+++ 04_Procedure/summary_data/pt_br_together.R
@@ -454,3 +454,204 @@ for (i in 1:number_folders){
   
 }
 
+# generate for COMBO ----
+combo_use <- subset(pt_merged, is.na(done_totalN) | done_totalN == FALSE) 
+combo_sample <- subset(pt_merged, done_totalN == TRUE) 
+
+# loop over the number of random folders you need
+# pt3, pt4, pt5
+# br_pt1, br_pt2, br_pt3
+folder_list <- c("pt3", "pt4", "pt5", "br_pt1", "br_pt2", "br_pt3")
+for (i in 1:length(folder_list)){
+  
+  folder_num <- folder_list[i]
+  
+  # eight blocks of 100 trials = 800 trials = 400 pairs or 8 blocks of 50
+  # 150 non word non word = 300 trials
+  if (nrow(combo_use[combo_use$cue_type == "nonword" &
+                  combo_use$target_type == "nonword", ]) >= 150){
+    
+    temp <- subset(combo_use,
+                   combo_use$cue_type == "nonword" &
+                     combo_use$target_type == "nonword")
+    nonwords <- temp[sample(1:nrow(temp), 150, replace = F), ]
+    
+  }else{
+    
+    nonwords <- combo_use[combo_use$cue_type == "nonword" &
+                         combo_use$target_type == "nonword", ]
+    temp <- subset(combo_sample,
+                   combo_use$cue_type == "nonword" &
+                     combo_use$target_type == "nonword")
+    nonwords <- rbind(nonwords,
+                      temp[sample(1:nrow(temp),
+                                  150-nrow(nonwords),
+                                  replace = F), ])
+  }
+  
+  # 100 non word non word = 200 trials
+  if (nrow(combo_use[(combo_use$cue_type == "nonword" &
+                   combo_use$target_type == "word") |
+                  (combo_use$cue_type == "word" &
+                   combo_use$target_type == "nonword"), ]) >= 100){
+    
+    temp <- subset(combo_use,
+                   (combo_use$cue_type == "nonword" &
+                      combo_use$target_type == "word") |
+                     (combo_use$cue_type == "word" &
+                        combo_use$target_type == "nonword"))
+    nonwords_mix <- temp[sample(1:nrow(temp), 100, replace = F), ]
+    
+  }else{
+    
+    nonwords_mix <- combo_use[(combo_use$cue_type == "nonword" &
+                              combo_use$target_type == "word") |
+                             (combo_use$cue_type == "word" &
+                                combo_use$target_type == "nonword"), ]
+    temp <- subset(combo_sample,
+                   (combo_use$cue_type == "nonword" &
+                      combo_use$target_type == "word") |
+                     (combo_use$cue_type == "word" &
+                        combo_use$target_type == "nonword"))
+    nonwords_mix <- rbind(nonwords_mix,
+                          temp[sample(1:nrow(temp),
+                                      100-nrow(nonwords_mix),
+                                      replace = F), ])
+  }
+  
+  # 75 related pairs = 150 trials
+  if (nrow(combo_use[combo_use$type == "related" , ]) >= 75){
+    
+    temp <- subset(combo_use, type == "related")
+    related <- temp[sample(1:nrow(temp), 75, replace = F), ]
+    
+  }else{
+    
+    related <- combo_use[combo_use$type == "related", ]
+    temp <- subset(combo_sample, type == "related")
+    related <- rbind(related,
+                     temp[sample(1:nrow(temp),
+                                 75-nrow(related),
+                                 replace = F), ])
+  }
+  
+  # 75 unrelated pairs = 150 trials
+  if (nrow(combo_use[combo_use$type == "unrelated" , ]) >= 75){
+    
+    temp <- subset(combo_use, type == "unrelated")
+    unrelated <- temp[sample(1:nrow(temp), 75, replace = F), ]
+    
+  }else{
+    
+    unrelated <- combo_use[combo_use$type == "unrelated", ]
+    temp <- subset(combo_sample, type == "unrelated")
+    unrelated <- rbind(unrelated,
+                       temp[sample(1:nrow(temp),
+                                   75-nrow(unrelated),
+                                   replace = F), ])
+  }
+  
+  # db6cc958e11fc3987cebacc1e14b253b95b4de4d05c702ecbb3294775adb3e4b.json is practice
+  
+  practice <- '[
+  {"word": "sneife", "class": "nonword"},
+  {"word": "lábios", "class": "word"},
+  {"word": "alcatrão", "class": "word"},
+  {"word": "sodol", "class": "nonword"},
+  {"word": "nwede", "class": "nonword"},
+  {"word": "presente", "class": "word"},
+  {"word": "kegthe", "class": "nonword"},
+  {"word": "tomada", "class": "word"},
+  {"word": "ethorar", "class": "nonword"},
+  {"word": "bota", "class": "word"}]'
+  
+  writeLines(practice, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/db6cc958e11fc3987cebacc1e14b253b95b4de4d05c702ecbb3294775adb3e4b.json"))
+  
+  all_trials <- rbind(nonwords, related, unrelated, nonwords_mix)
+  all_trials <- all_trials[sample(1:nrow(all_trials), nrow(all_trials), replace = F), ]
+  all_trials$together <- paste('{"word": "',
+                               all_trials$combo_cue,
+                               '", "class": "',
+                               all_trials$cue_type,
+                               '"}, ', #cue
+                               '{"word": "',
+                               all_trials$combo_target,
+                               '", "class": "',
+                               all_trials$target_type,
+                               '"}', sep = "")
+  
+  
+  # 3cee33bcfe0a7bdac59ec1374ca41a4ea7fe6e772c9b0ab0770f0d1f5cb09e41.json is real1
+  
+  # remember these come in pairs
+  real <- paste('[',
+                paste(all_trials$together[1:50], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/3cee33bcfe0a7bdac59ec1374ca41a4ea7fe6e772c9b0ab0770f0d1f5cb09e41.json"))
+  
+  # ae2c5987efa101760004c66c0da975c7dd75605ada53cabf75ec439ce68a5871.json is real2
+  
+  real <- paste('[',
+                paste(all_trials$together[51:100], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/ae2c5987efa101760004c66c0da975c7dd75605ada53cabf75ec439ce68a5871.json"))
+  
+  # 3a95e1234833448efe1e098102f00e2f4bb85d6edd8b6a093f62a93d4dcf4f4e.json is real3
+  
+  real <- paste('[',
+                paste(all_trials$together[101:150], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/3a95e1234833448efe1e098102f00e2f4bb85d6edd8b6a093f62a93d4dcf4f4e.json"))
+  
+  # 994ac7a5038c8713adb715e04d6639acda5d02a40abdb81d59c0d39dfea6cf06.json is real4
+  
+  real <- paste('[',
+                paste(all_trials$together[151:200], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/994ac7a5038c8713adb715e04d6639acda5d02a40abdb81d59c0d39dfea6cf06.json"))
+  
+  # 9febe5343449a1c79d42f597f494397c595dd944600a7908e38167bbb18234ee.json is real5
+  
+  real <- paste('[',
+                paste(all_trials$together[201:250], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/9febe5343449a1c79d42f597f494397c595dd944600a7908e38167bbb18234ee.json"))
+  
+  # cd99c6e5b4b714268551fce4fc08729821a7bdb4a6f2294152b2e0d5e4ddfb99.json is real6
+  real <- paste('[',
+                paste(all_trials$together[251:300], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/cd99c6e5b4b714268551fce4fc08729821a7bdb4a6f2294152b2e0d5e4ddfb99.json"))
+  
+  # c378cfb94011283fa98a84e5e2d34272f4a3134cda08298ed211f9c6c2331757.json is real7
+  real <- paste('[',
+                paste(all_trials$together[301:350], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/c378cfb94011283fa98a84e5e2d34272f4a3134cda08298ed211f9c6c2331757.json"))
+  
+  # 0d00e4cacc8fbd59aa34a45be41f535ccade17517701d1b3fa6ef139ca8746a3.json is real8
+  real <- paste('[',
+                paste(all_trials$together[351:400], collapse = ",", sep = ""),
+                ']', collapse = "", sep = "")
+  writeLines(real, con = paste0(
+    "/var/www/html/", folder_num,
+    "/embedded/0d00e4cacc8fbd59aa34a45be41f535ccade17517701d1b3fa6ef139ca8746a3.json"))
+  
+}
+
